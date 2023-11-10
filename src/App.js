ab28bac20b316b5201deb7aca01fea51706037ef
Jani Kaikkonen <jh.kaikkonen@gmail.com>
2023-11-10 13:20:09 +0200

    Contiuning work

--- src/App.js
+++ src/App.js
@@ -28,7 +28,36 @@ function reducer(state, { type, payload }) {
         ...state,
         currentOperand: `${state.currentOperand || ""}${action.payload.digit}`,
       }
+      case ACTIONS.CHOOSE_OPERATION:
+          if (state.currentOperand == null && state.previousOperand == null) {
+            return state
+          }
+
+          if (state.previousOperand == null ) {
+            return {
+              ...state,
+              operation: payload.operation,
+              previousOperand: state.currentOperand
+              currentOperand: null,
+            }
+          }
+
+          return {
+            ...state,
+            previousOperand: evaluate(state),
+            operation: payload.operation,
+            currentOperand: null
+          }
+      case ACTIONS.CLEAR: 
+        return {}
   }
+}
+
+function evaluate({ currentOperand, previousOperand, operation}) {
+  const prev = parseFloat(previousOperand)
+  const prev = parseFloat(currentOperand)
+  if (isNaN(prev)) || isNaN=(current)) return ""
+  let compuation
 }
 function App() {
   const [{ currentOperand, previousOperand, operation }, dispatch] = useReducer(
@@ -40,7 +69,9 @@ function App() {
         <div className='previous-operand'>{previousOperand} {operation}</div>
         <div className='current-operand'>{currentOperand}</div>
       </div>
-      <button className='span-two'>AC</button>
+      <button className='span-two' onClick={() => dispatch({ type: ACTIONS.CLEAR })}
+      >
+        AC</button>
       <button>DEL</button>
       {/*<DigitButton digit="/" dispatch={dispatch} />*/}
       <OperationButton operation="/" dispatch{dispatch} />
