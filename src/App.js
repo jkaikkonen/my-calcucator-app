f0c70079f14a3b05f8d85fcfb0cf77e8f7ac6385
Jani Kaikkonen <jh.kaikkonen@gmail.com>
2023-11-15 10:44:00 +0200

    Bug fixes

--- src/App.js
+++ src/App.js
@@ -37,8 +37,8 @@ function reducer(state, { type, payload }) {
             return {
               ...state,
               operation: payload.operation,
-              previousOperand: state.currentOperand
-              currentOperand: null,
+              previousOperand: state.currentOperand,
+              currentOperand: null
             }
           }
 
@@ -55,9 +55,24 @@ function reducer(state, { type, payload }) {
 
 function evaluate({ currentOperand, previousOperand, operation}) {
   const prev = parseFloat(previousOperand)
-  const prev = parseFloat(currentOperand)
-  if (isNaN(prev)) || isNaN=(current)) return ""
-  let compuation
+  const current = parseFloat(currentOperand)
+  if (isNaN(prev) || isNaN(current)) return ""
+  let compuation = ""
+  switch (operation) {
+    case "+":
+      compuation = prev + current
+      break
+    case "-":
+      compuation = prev - current
+      break 
+    case "/":
+      compuation = prev / current
+    case "*":
+      computation = prev * current
+      break
+  }
+return compuation.toString()
+
 }
 function App() {
   const [{ currentOperand, previousOperand, operation }, dispatch] = useReducer(
@@ -74,19 +89,19 @@ function App() {
         AC</button>
       <button>DEL</button>
       {/*<DigitButton digit="/" dispatch={dispatch} />*/}
-      <OperationButton operation="/" dispatch{dispatch} />
+      <OperationButton operation="/" dispatch={dispatch} />
       <DigitButton digit="1" dispatch={dispatch} />
       <DigitButton digit="2" dispatch={dispatch} />
       <DigitButton digit="3" dispatch={dispatch} />
-      <OperationButton operation="*" dispatch{dispatch} />
+      <OperationButton operation="*" dispatch={dispatch} />
       <DigitButton digit="4" dispatch={dispatch} />
       <DigitButton digit="5" dispatch={dispatch} />
       <DigitButton digit="6" dispatch={dispatch} />
-      <OperationButton operation="+" dispatch{dispatch} />
+      <OperationButton operation="+" dispatch={dispatch} />
       <DigitButton digit="7" dispatch={dispatch} />
       <DigitButton digit="8" dispatch={dispatch} />
       <DigitButton digit="9" dispatch={dispatch} />
-      <OperationButton operation="-" dispatch{dispatch} />
+      <OperationButton operation="-" dispatch={dispatch} />
       <DigitButton digit="." dispatch={dispatch} />
       <DigitButton digit="0" dispatch={dispatch} />
       <button className='span-two'>=</button>
